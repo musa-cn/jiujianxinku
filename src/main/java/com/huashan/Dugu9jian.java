6eb2c07b6476692101d0be4a0eb0d4c58d678a44
yuebuqun <1113570106@qq.com>
2021-02-27 14:33:46 +0800

    yuebuqun冲突

--- src/main/java/com/huashan/Dugu9jian.java
+++ src/main/java/com/huashan/Dugu9jian.java
@@ -4,6 +4,6 @@ public class Dugu9jian {
     public static void main(String[] args) {
         System.out.println("传到远端");
         System.out.println("修改");
-        System.out.println("冲突");
+        System.out.println("冲突a");
     }
 }
